c725f4f1803d33c0712532bf93e4a741f1c92597
Parham <parhamk75@hotmail.com>
2020-09-10 11:44:20 +0430

    DEV: Delay, Write, Read outline added + Mode flag added to GLCD Structure > Timing and pin configurations in GLCD_L0 Functions

--- Inc/GLCD_L0.h
+++ Inc/GLCD_L0.h
@@ -6,20 +6,30 @@
 // Includes
 #include "stm32f4xx_hal.h"
 
-// Timing Parameters [in nano Seconds]
-#define GLCD_L0_T_C     ((uint16_t)1000)        // E Cycle
-#define GLCD_L0_T_WH    ((uint16_t)450)         // E High Level Width
-#define GLCD_L0_T_WL    ((uint16_t)450)         // E Low Level Width
-#define GLCD_L0_T_R     ((uint16_t)25)          // E Rise Time <Max>
-#define GLCD_L0_T_F     ((uint16_t)25)          // E Fall Time <Max>
-#define GLCD_L0_T_ASU   ((uint16_t)140)         // Address Set-Up Time
-#define GLCD_L0_T_AH    ((uint16_t)10)          // Address Hold Time
-#define GLCD_L0_T_SU    ((uint16_t)200)         // Data Set-Up Time
-#define GLCD_L0_T_D     ((uint16_t)320)         // Data Delay Time <Max>
-#define GLCD_L0_T_DHW   ((uint16_t)10)          // Data Hold Time (Write)
-#define GLCD_L0_T_DHR   ((uint16_t)20)          // Data Delay Time (Read)
+// MPU Definitions
+#define GLCD_L0_MPU_10NS_CNST   ((uint8_t)1)          // MPU Clock Period in 10x nanosecond
 
+// Timing Parameters [in 10x nano Seconds]
+#define GLCD_L0_T_C             ((uint8_t)100)        // E Cycle <Min>
+#define GLCD_L0_T_WH            ((uint8_t)45)         // E High Level Width <Min>
+#define GLCD_L0_T_WL            ((uint8_t)45)         // E Low Level Width <Min>
+#define GLCD_L0_T_R             ((uint8_t)2)          // E Rise Time <Min>
+#define GLCD_L0_T_F             ((uint8_t)2)          // E Fall Time <Min>
+#define GLCD_L0_T_ASU           ((uint8_t)14)         // Address Set-Up Time <Min>
+#define GLCD_L0_T_AH            ((uint8_t)1)          // Address Hold Time <Min>
+#define GLCD_L0_T_SU            ((uint8_t)20)         // Data Set-Up Time <Min>
+#define GLCD_L0_T_D             ((uint8_t)32)         // Data Delay Time <Min>
+#define GLCD_L0_T_DHW           ((uint8_t)1)          // Data Hold Time (Write) <Min>
+#define GLCD_L0_T_DHR           ((uint8_t)2)          // Data Delay Time (Read) <Min>
+
+typedef enum{
+    GLCD_L0_Mode_Read = 0,
+    GLCD_L0_Mode_Wite = 1
+}GLCD_L0_MODE_TypeDef;
+
+// The GLCD Pinout typedef structure
 typedef struct{
+    // GPIO Ports
     GPIO_TypeDef* DB_Ports[8];
     GPIO_TypeDef* EN_Port;
     GPIO_TypeDef* RS_Port;
@@ -28,13 +38,23 @@ typedef struct{
     GPIO_TypeDef* CS1_Port;
     GPIO_TypeDef* CS2_Port;
 
+    // GPIO Pins
     uint16_t DB_Pins[8];
     uint16_t EN_Pin;
     uint16_t RS_Pin;
     uint16_t RW_Pin;
     uint16_t RST_Pin;
     uint16_t CS1_Pin;
-    uint16_t CS2_Pin;	
+    uint16_t CS2_Pin;
+
+    // GLCD Last Mode Flag (Read/Write)
+    GLCD_L0_MODE_TypeDef Mode;
+    
 }GLCD_L0_TypeDef;
+
+HAL_StatusTypeDef   GLCD_L0_Delay(uint16_t td_10xns_);
+HAL_StatusTypeDef   GLCD_L0_Write(GLCD_L0_TypeDef* pglcd_, uint8_t DBs_, uint8_t is_instrctn_);  //TODO: implement flags with a boolean data type
+uint8_t             GLCD_L0_Read(GLCD_L0_TypeDef* pglcd_, uint8_t DBs_, uint8_t is_instrctn_);   //TODO: implement flags with a boolean data type
+HAL_StatusTypeDef   GLCD_L0_Reset(GLCD_L0_TypeDef* pglcd_);
 
 #endif  //_GLCD_L0_H
--- Src/GLCD_L0.c
+++ Src/GLCD_L0.c
@@ -1,1 +1,88 @@
 #include "GLCD_L0.h"
+
+
+
+HAL_StatusTypeDef GLCD_L0_Delay(uint16_t td_10xns_)
+{
+
+    for (uint16_t i = td_10xns_; i > 0; i--)
+    {
+        for(uint8_t j = GLCD_L0_MPU_10NS_CNST; j > 1; j--);
+    }
+    
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef GLCD_L0_Write(GLCD_L0_TypeDef* pglcd_, uint8_t DBs_, uint8_t is_instrctn_)
+{
+    // Re-Configurate DB Pins
+    // EN -> Low (For Address Set-Up)
+    HAL_GPIO_WritePin(pglcd_->EN_Port, pglcd_->EN_Pin, GPIO_PIN_RESET);
+    // RW -> Low
+    HAL_GPIO_WritePin(pglcd_->RW_Port, pglcd_->RW_Pin, GPIO_PIN_RESET);
+    // RS -> Set
+    HAL_GPIO_WritePin(pglcd_->RS_Port, pglcd_->RS_Pin, (GPIO_PinState)is_instrctn_);
+    // CS1, CS2 -> Low
+    HAL_GPIO_WritePin(pglcd_->CS1_Port, pglcd_->CS1_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(pglcd_->CS2_Port, pglcd_->CS2_Pin, GPIO_PIN_RESET);
+    // DBs -> Set
+    for(uint8_t i = 0; i < 8; i++)
+    {
+        HAL_GPIO_WritePin(pglcd_->DB_Ports[i], pglcd_->DB_Pins[i], (GPIO_PinState)((DBs_>>i)%2));
+    }
+    // EN -> High (Prepare for a Falling Edge)
+    HAL_GPIO_WritePin(pglcd_->EN_Port, pglcd_->EN_Pin, GPIO_PIN_SET);
+    // EN -> Low (Make a Falling Edge)
+    HAL_GPIO_WritePin(pglcd_->EN_Port, pglcd_->EN_Pin, GPIO_PIN_RESET);
+    // EN, RW, CS1, CS2 -> High (Terminate the Job)
+    HAL_GPIO_WritePin(pglcd_->EN_Port, pglcd_->EN_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(pglcd_->RW_Port, pglcd_->RW_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(pglcd_->CS1_Port, pglcd_->CS1_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(pglcd_->CS2_Port, pglcd_->CS2_Pin, GPIO_PIN_SET);
+    // RS -> Reset
+    HAL_GPIO_TogglePin(pglcd_->RS_Port, pglcd_->RS_Pin);
+    
+    return HAL_OK;
+}
+
+uint8_t GLCD_L0_Read(GLCD_L0_TypeDef* pglcd_, uint8_t DBs_, uint8_t is_instrctn_)
+{
+    // Re-Configurate DB Pins
+    // EN -> Low (For Address Set-Up)
+    HAL_GPIO_WritePin(pglcd_->EN_Port, pglcd_->EN_Pin, GPIO_PIN_RESET);
+    // RW -> High
+    HAL_GPIO_WritePin(pglcd_->RW_Port, pglcd_->RW_Pin, GPIO_PIN_SET);
+    // RS -> Set
+    HAL_GPIO_WritePin(pglcd_->RS_Port, pglcd_->RS_Pin, (GPIO_PinState)is_instrctn_);
+    // CS1, CS2 -> Low
+    HAL_GPIO_WritePin(pglcd_->CS1_Port, pglcd_->CS1_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(pglcd_->CS2_Port, pglcd_->CS2_Pin, GPIO_PIN_RESET);
+    // EN -> High (Prepare for a Falling Edge)
+    HAL_GPIO_WritePin(pglcd_->EN_Port, pglcd_->EN_Pin, GPIO_PIN_SET);
+    // DBs -> Read
+    uint8_t tmp_val = 0x00;
+    for(uint8_t i = 0; i < 8; i++)
+    {
+        tmp_val |= HAL_GPIO_ReadPin(pglcd_->DB_Ports[i], pglcd_->DB_Pins[i]) << i;
+    }
+    // EN -> Low (Make a Falling Edge)
+    HAL_GPIO_WritePin(pglcd_->EN_Port, pglcd_->EN_Pin, GPIO_PIN_RESET);
+    // RW -> Low
+    HAL_GPIO_WritePin(pglcd_->RW_Port, pglcd_->RW_Pin, GPIO_PIN_RESET);
+    // CS1, CS2 -> High (Terminate the Job)
+    HAL_GPIO_WritePin(pglcd_->CS1_Port, pglcd_->CS1_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(pglcd_->CS2_Port, pglcd_->CS2_Pin, GPIO_PIN_SET);
+    // RS -> Reset
+    HAL_GPIO_TogglePin(pglcd_->RS_Port, pglcd_->RS_Pin);
+    
+    return tmp_val;
+}
+
+
+HAL_StatusTypeDef   GLCD_L0_Reset(GLCD_L0_TypeDef* pglcd_)
+{
+    
+
+    return HAL_OK;
+}
+
