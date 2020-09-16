abf37e77368ec30f789b6eab081894de3f0474b0
Parham <parhamk75@hotmail.com>
2020-09-16 15:44:03 +0430

    DEV: ported to FreeRTOS and tested

--- Src/main.c
+++ Src/main.c
@@ -19,6 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "cmsis_os.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -47,31 +48,32 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-GLCD_L0_TypeDef tmp_glcd;
+// GLCD_L0_TypeDef tmp_glcd;
 
 
-// Temporary Variables for Debugging
-char tmp_str[] = "12345678901234567890";
-uint8_t tmp_cntr = 0;
+// // Temporary Variables for Debugging
+// char tmp_str[] = "12345678901234567890";
+// uint8_t tmp_cntr = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+void MX_FREERTOS_Init(void);
 /* USER CODE BEGIN PFP */
-void tmp_wait_for_busy(GLCD_L0_HALF_TypeDef hlf_)
-{
-  while(GLCD_L2_IsDispBusy(&tmp_glcd, hlf_) == GLCD_L2_DispStatBusy_Busy)
-  {
-    HAL_UART_Transmit_IT(&huart2, (uint8_t*)"Hi\r\n", 4);
-  }
-}
-void tmp_check_status(void)
-{
-  sprintf(tmp_str, "R%02X\r\n", GLCD_L2_ReadDispStatus(&tmp_glcd, GLCD_L2_HALF_Right));
-  HAL_UART_Transmit(&huart2, (uint8_t*)tmp_str, 5, 200);
-  sprintf(tmp_str, "L%02X\r\n", GLCD_L2_ReadDispStatus(&tmp_glcd, GLCD_L2_HALF_Left));
-  HAL_UART_Transmit(&huart2, (uint8_t*)tmp_str, 5, 200);
-}
+// void tmp_wait_for_busy(GLCD_L0_HALF_TypeDef hlf_)
+// {
+//   while(GLCD_L2_IsDispBusy(&tmp_glcd, hlf_) == GLCD_L2_DispStatBusy_Busy)
+//   {
+//     HAL_UART_Transmit_IT(&huart2, (uint8_t*)"Hi\r\n", 4);
+//   }
+// }
+// void tmp_check_status(void)
+// {
+//   sprintf(tmp_str, "R%02X\r\n", GLCD_L2_ReadDispStatus(&tmp_glcd, GLCD_L2_HALF_Right));
+//   HAL_UART_Transmit(&huart2, (uint8_t*)tmp_str, 5, 200);
+//   sprintf(tmp_str, "L%02X\r\n", GLCD_L2_ReadDispStatus(&tmp_glcd, GLCD_L2_HALF_Left));
+//   HAL_UART_Transmit(&huart2, (uint8_t*)tmp_str, 5, 200);
+// }
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -109,92 +111,100 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-  tmp_glcd.DB_Ports[0] = LCD_D0_GPIO_Port;
-  tmp_glcd.DB_Ports[1] = LCD_D1_GPIO_Port;
-  tmp_glcd.DB_Ports[2] = LCD_D2_GPIO_Port;
-  tmp_glcd.DB_Ports[3] = LCD_D3_GPIO_Port;
-  tmp_glcd.DB_Ports[4] = LCD_D4_GPIO_Port;
-  tmp_glcd.DB_Ports[5] = LCD_D5_GPIO_Port;
-  tmp_glcd.DB_Ports[6] = LCD_D6_GPIO_Port;
-  tmp_glcd.DB_Ports[7] = LCD_D7_GPIO_Port;
+  // tmp_glcd.DB_Ports[0] = LCD_D0_GPIO_Port;
+  // tmp_glcd.DB_Ports[1] = LCD_D1_GPIO_Port;
+  // tmp_glcd.DB_Ports[2] = LCD_D2_GPIO_Port;
+  // tmp_glcd.DB_Ports[3] = LCD_D3_GPIO_Port;
+  // tmp_glcd.DB_Ports[4] = LCD_D4_GPIO_Port;
+  // tmp_glcd.DB_Ports[5] = LCD_D5_GPIO_Port;
+  // tmp_glcd.DB_Ports[6] = LCD_D6_GPIO_Port;
+  // tmp_glcd.DB_Ports[7] = LCD_D7_GPIO_Port;
 
-  tmp_glcd.DB_Pins[0] = LCD_D0_Pin;
-  tmp_glcd.DB_Pins[1] = LCD_D1_Pin;
-  tmp_glcd.DB_Pins[2] = LCD_D2_Pin;
-  tmp_glcd.DB_Pins[3] = LCD_D3_Pin;
-  tmp_glcd.DB_Pins[4] = LCD_D4_Pin;
-  tmp_glcd.DB_Pins[5] = LCD_D5_Pin;
-  tmp_glcd.DB_Pins[6] = LCD_D6_Pin;
-  tmp_glcd.DB_Pins[7] = LCD_D7_Pin;
+  // tmp_glcd.DB_Pins[0] = LCD_D0_Pin;
+  // tmp_glcd.DB_Pins[1] = LCD_D1_Pin;
+  // tmp_glcd.DB_Pins[2] = LCD_D2_Pin;
+  // tmp_glcd.DB_Pins[3] = LCD_D3_Pin;
+  // tmp_glcd.DB_Pins[4] = LCD_D4_Pin;
+  // tmp_glcd.DB_Pins[5] = LCD_D5_Pin;
+  // tmp_glcd.DB_Pins[6] = LCD_D6_Pin;
+  // tmp_glcd.DB_Pins[7] = LCD_D7_Pin;
 
-  tmp_glcd.CS1_Port = LCD_CS1_GPIO_Port;
-  tmp_glcd.CS1_Pin  = LCD_CS1_Pin;
+  // tmp_glcd.CS1_Port = LCD_CS1_GPIO_Port;
+  // tmp_glcd.CS1_Pin  = LCD_CS1_Pin;
   
-  tmp_glcd.CS2_Port = LCD_CS2_GPIO_Port;
-  tmp_glcd.CS2_Pin  = LCD_CS2_Pin;
+  // tmp_glcd.CS2_Port = LCD_CS2_GPIO_Port;
+  // tmp_glcd.CS2_Pin  = LCD_CS2_Pin;
   
-  tmp_glcd.RS_Port  = LCD_RS_GPIO_Port;
-  tmp_glcd.RS_Pin   = LCD_RS_Pin;
+  // tmp_glcd.RS_Port  = LCD_RS_GPIO_Port;
+  // tmp_glcd.RS_Pin   = LCD_RS_Pin;
   
-  tmp_glcd.RW_Port  = LCD_RW_GPIO_Port;
-  tmp_glcd.RW_Pin   = LCD_RW_Pin;
+  // tmp_glcd.RW_Port  = LCD_RW_GPIO_Port;
+  // tmp_glcd.RW_Pin   = LCD_RW_Pin;
   
-  tmp_glcd.EN_Port  = LCD_EN_GPIO_Port;
-  tmp_glcd.EN_Pin   = LCD_EN_Pin;
+  // tmp_glcd.EN_Port  = LCD_EN_GPIO_Port;
+  // tmp_glcd.EN_Pin   = LCD_EN_Pin;
   
-  tmp_glcd.RST_Port = LCD_RST_GPIO_Port;
-  tmp_glcd.RST_Pin  = LCD_RST_Pin;
+  // tmp_glcd.RST_Port = LCD_RST_GPIO_Port;
+  // tmp_glcd.RST_Pin  = LCD_RST_Pin;
 
-  tmp_glcd.Mode     = GLCD_L0_Mode_Write;
+  // tmp_glcd.Mode     = GLCD_L0_Mode_Write;
 
-  // Some Tests for GLCD_L0
-  HAL_UART_Transmit_IT(&huart2, (uint8_t*)"LCD Init Finished\r\n", 19);
+  // // Some Tests for GLCD_L0
+  // HAL_UART_Transmit_IT(&huart2, (uint8_t*)"LCD Init Finished\r\n", 19);
   
-  // Perform a Reset procedure
-  GLCD_L2_FullInit(&tmp_glcd, GLCD_L2_DispColor_Black);
+  // // Perform a Reset procedure
+  // GLCD_L2_FullInit(&tmp_glcd, GLCD_L2_DispColor_Black);
 
-  // Turning on and off and read the Status Reg
-  GLCD_L1_Disp_OnOff(&tmp_glcd, GLCD_L0_HALF_Right, GLCD_L1_Disp_On); // LCD On
-  GLCD_L1_Disp_OnOff(&tmp_glcd, GLCD_L0_HALF_Left, GLCD_L1_Disp_Off); // LCD Off
-  tmp_check_status();
-  GLCD_L1_Disp_OnOff(&tmp_glcd, GLCD_L0_HALF_Left, GLCD_L1_Disp_On); // LCD On
-  HAL_Delay(1000);
+  // // Turning on and off and read the Status Reg
+  // GLCD_L1_Disp_OnOff(&tmp_glcd, GLCD_L0_HALF_Right, GLCD_L1_Disp_On); // LCD On
+  // GLCD_L1_Disp_OnOff(&tmp_glcd, GLCD_L0_HALF_Left, GLCD_L1_Disp_Off); // LCD Off
+  // tmp_check_status();
+  // GLCD_L1_Disp_OnOff(&tmp_glcd, GLCD_L0_HALF_Left, GLCD_L1_Disp_On); // LCD On
+  // HAL_Delay(1000);
 
-  tmp_check_status();
+  // tmp_check_status();
 
-  // Ser Address Regs
-  GLCD_L2_GotoXYZ(&tmp_glcd, GLCD_L2_HALF_Both, 0, 0, 0);
+  // // Ser Address Regs
+  // GLCD_L2_GotoXYZ(&tmp_glcd, GLCD_L2_HALF_Both, 0, 0, 0);
 
-  for (size_t j = 0; j < 8; j++)
-  {
-    for (size_t i = 0; i < 64; i++)
-    {
-      GLCD_L2_WriteByteXYZ(&tmp_glcd, GLCD_L2_HALF_Both, (uint8_t)(0x55), j, i, i>>1);
-      HAL_Delay(45);
-    }
-  }
-  HAL_Delay(2000);
-  GLCD_L2_ClearDisplay(&tmp_glcd, GLCD_L2_HALF_Both);
-  HAL_Delay(2000);
+  // for (size_t j = 0; j < 8; j++)
+  // {
+  //   for (size_t i = 0; i < 64; i++)
+  //   {
+  //     GLCD_L2_WriteByteXYZ(&tmp_glcd, GLCD_L2_HALF_Both, (uint8_t)(0x55), j, i, i>>1);
+  //     HAL_Delay(45);
+  //   }
+  // }
+  // HAL_Delay(2000);
+  // GLCD_L2_ClearDisplay(&tmp_glcd, GLCD_L2_HALF_Both);
+  // HAL_Delay(2000);
   /* USER CODE END 2 */
 
+  /* Init scheduler */
+  osKernelInitialize();  /* Call init function for freertos objects (in freertos.c) */
+  MX_FREERTOS_Init();
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    tmp_cntr++;
-    GLCD_L2_GotoZ(&tmp_glcd, GLCD_L2_HALF_Both, tmp_cntr%64);
-    // Show image on LCD
-    for (size_t j = 0; j < 4; j++)
-    {  
-      GLCD_L2_GotoX(&tmp_glcd, GLCD_L2_HALF_Both, j); // x  
-      tmp_wait_for_busy(GLCD_L0_HALF_Right);
-      for (size_t i = 0; i < 64; i++)
-      {
-        GLCD_L2_WriteByte(&tmp_glcd, GLCD_L2_HALF_Left, (uint8_t)(i+ tmp_cntr));
-        GLCD_L2_WriteByte(&tmp_glcd, GLCD_L2_HALF_Right, (uint8_t)(i+ tmp_cntr+ 64));        
-      }
-    }
+    
+    // tmp_cntr++;
+    // GLCD_L2_GotoZ(&tmp_glcd, GLCD_L2_HALF_Both, tmp_cntr%64);
+    // // Show image on LCD
+    // for (size_t j = 0; j < 4; j++)
+    // {  
+    //   GLCD_L2_GotoX(&tmp_glcd, GLCD_L2_HALF_Both, j); // x  
+    //   tmp_wait_for_busy(GLCD_L0_HALF_Right);
+    //   for (size_t i = 0; i < 64; i++)
+    //   {
+    //     GLCD_L2_WriteByte(&tmp_glcd, GLCD_L2_HALF_Left, (uint8_t)(i+ tmp_cntr));
+    //     GLCD_L2_WriteByte(&tmp_glcd, GLCD_L2_HALF_Right, (uint8_t)(i+ tmp_cntr+ 64));        
+    //   }
+    // }
 
     /*
     // Blink the LED once
@@ -259,6 +269,27 @@ void SystemClock_Config(void)
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM11 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM11) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
 
 /**
   * @brief  This function is executed in case of error occurrence.
