975d66b6be2f489b103423c2dd6de6ec2a70dca3
Parham <parhamk75@hotmail.com>
2020-11-21 15:48:34 +0330

    FIX: GLCD_L4_T: WriteStringXY() supports 32, 16 and 8 bit data & tested + WriteCharXY() supports 32bit values

--- Inc/GLCD_L4_T.h
+++ Inc/GLCD_L4_T.h
@@ -5,14 +5,19 @@
 #include "stdint.h"
 #include "stdbool.h"
 
+typedef enum{
+    GLCD_L4_T_Data_8bit   = (uint_fast8_t)0,
+    GLCD_L4_T_Data_16bit  = (uint_fast8_t)1,
+    GLCD_L4_T_Data_32bit  = (uint_fast8_t)2
+}GLCD_L4_Data_TypeDef;
 
 // The GLCD Pinout typedef structure
 typedef struct{
     // Properties
     uint8_t     width;                  /* General Width of Characters */
     uint8_t     height;                 /* General Height of Characters */
-    uint16_t    first_char_num;         /* First Character Number (Index) in the Font_Data Array */
-    uint16_t    last_char_num;          /* Last Character Number (Index) in the Font_Data Array */
+    uint32_t    first_char_num;         /* First Character Number (Index) in the Font_Data Array */
+    uint32_t    last_char_num;          /* Last Character Number (Index) in the Font_Data Array */
 
     // Data
     const uint8_t* undifined_char_data; /* Pointer to the Font Data for Undifined Character Data Array in the STD Form */
@@ -21,8 +26,8 @@ typedef struct{
 }GLCD_L4_T_Font_TypeDef;
 
 // Normal Write Funcs
-HAL_StatusTypeDef   GLCD_L4_T_WriteCharXY   (GLCD_L0_TypeDef* pglcd_, uint16_t char_, GLCD_L4_T_Font_TypeDef* font_, uint8_t x_, uint8_t y_);
-HAL_StatusTypeDef   GLCD_L4_T_WriteStringXY (GLCD_L0_TypeDef* pglcd_, uint16_t* str_, GLCD_L4_T_Font_TypeDef* font_, uint8_t x_, uint8_t y_, uint16_t len_, bool one_line_, bool cut_off_);
+HAL_StatusTypeDef   GLCD_L4_T_WriteCharXY   (GLCD_L0_TypeDef* pglcd_, uint32_t char_, GLCD_L4_T_Font_TypeDef* font_, uint8_t x_, uint8_t y_);
+HAL_StatusTypeDef   GLCD_L4_T_WriteStringXY (GLCD_L0_TypeDef* pglcd_, void* str_, GLCD_L4_Data_TypeDef dt_, GLCD_L4_T_Font_TypeDef* font_, uint8_t x_, uint8_t y_, uint16_t len_, bool one_line_, bool cut_off_);
 
 HAL_StatusTypeDef   GLCD_L4_T_SetNoLines        (uint8_t no_lines_);
 HAL_StatusTypeDef   GLCD_L4_T_WriteCharLine     (GLCD_L0_TypeDef* pglcd_, uint16_t char_, GLCD_L4_T_Font_TypeDef* font_, uint8_t line_, uint8_t y_);
--- Src/GLCD_L4_T.c
+++ Src/GLCD_L4_T.c
@@ -5,45 +5,63 @@ uint8_t GLCD_L4_T_No_Lines = 8;
 
 
 // Normal Write Funcs
-HAL_StatusTypeDef   GLCD_L4_T_WriteCharXY(GLCD_L0_TypeDef* pglcd_, uint16_t char_, GLCD_L4_T_Font_TypeDef* font_, uint8_t x_, uint8_t y_)
+HAL_StatusTypeDef   GLCD_L4_T_WriteCharXY(GLCD_L0_TypeDef* pglcd_, uint32_t char_, GLCD_L4_T_Font_TypeDef* font_, uint8_t x_, uint8_t y_)
 {
-    const uint8_t* tmp_pdata;
+    const uint8_t* tmp_pfdata;
     uint16_t no_bytes = font_->width *( (font_->height/8) + (font_->height %8 > 0 ? 1 : 0) );
 
     // Check if it is available in font data
     if(char_ > font_->last_char_num || char_ < font_->first_char_num)
     {
-        tmp_pdata = font_->undifined_char_data;
+        tmp_pfdata = font_->undifined_char_data;
     }
     else
     {
-        tmp_pdata = font_->font_data + ((char_-font_->first_char_num) * (no_bytes+1));
+        tmp_pfdata = font_->font_data + ((char_-font_->first_char_num) * (no_bytes+1));
     }
 
-    return GLCD_L3_WriteBitmap(pglcd_, tmp_pdata+1, x_, y_, *(tmp_pdata), font_->height, false);
+    return GLCD_L3_WriteBitmap(pglcd_, tmp_pfdata+1, x_, y_, *(tmp_pfdata), font_->height, false);
 }
 
 
-HAL_StatusTypeDef   GLCD_L4_T_WriteStringXY(GLCD_L0_TypeDef* pglcd_, uint16_t* str_, GLCD_L4_T_Font_TypeDef* font_, uint8_t x_, uint8_t y_, uint16_t len_, bool one_line_, bool cut_off_)
+HAL_StatusTypeDef   GLCD_L4_T_WriteStringXY(GLCD_L0_TypeDef* pglcd_, void* str_, GLCD_L4_Data_TypeDef dt_, GLCD_L4_T_Font_TypeDef* font_, uint8_t x_, uint8_t y_, uint16_t len_, bool one_line_, bool cut_off_)
 {
-    const uint8_t* tmp_pdata;
+
+    uint32_t* tmp_pstr_32 = (dt_ == GLCD_L4_T_Data_32bit ? (uint32_t*) str_ : NULL);
+    uint16_t* tmp_pstr_16 = (dt_ == GLCD_L4_T_Data_16bit ? (uint16_t*) str_ : NULL);
+    uint8_t*  tmp_pstr_8  = (dt_ == GLCD_L4_T_Data_8bit  ? (uint8_t*)  str_ : NULL);
+
+    const uint8_t* tmp_pfdata;
     uint16_t no_bytes = font_->width *( (font_->height/8) + (font_->height %8 > 0 ? 1 : 0) );
-    HAL_StatusTypeDef tmp_stat = HAL_OK;
 
     for(uint16_t cntr_char = 0; cntr_char < len_; cntr_char++)
     {
-        // Check if it is available in font data
-        if(*str_ > font_->last_char_num || *str_ < font_->first_char_num)
+        uint32_t tmp_char_val = 0;
+        if(dt_==GLCD_L4_T_Data_32bit)
         {
-            tmp_pdata = font_->undifined_char_data;
+            tmp_char_val = *(tmp_pstr_32++);
+        }
+        else if(dt_==GLCD_L4_T_Data_16bit)
+        {
+            tmp_char_val = *(tmp_pstr_16++);
         }
         else
         {
-            tmp_pdata = font_->font_data + (((*str_)-font_->first_char_num) * (no_bytes+1));
+            tmp_char_val = *(tmp_pstr_8++);
+        }
+
+        // Check if it is available in font data
+        if(tmp_char_val > font_->last_char_num || tmp_char_val < font_->first_char_num)
+        {
+            tmp_pfdata = font_->undifined_char_data;
+        }
+        else
+        {
+            tmp_pfdata = font_->font_data + ((tmp_char_val-font_->first_char_num) * (no_bytes+1));
         }
 
         // Checking for the EOL Problems (X)
-        if(x_ + (*tmp_pdata) > GLCD_L3_X_MAX)
+        if(x_ + (*tmp_pfdata) > GLCD_L3_X_MAX)
         {
             if(cut_off_)
             {
@@ -70,21 +88,28 @@ HAL_StatusTypeDef   GLCD_L4_T_WriteStringXY(GLCD_L0_TypeDef* pglcd_, uint16_t* s
             }
             else
             {
-                y_ = 0;
+                if(one_line_)
+                {
+                    return HAL_ERROR;
+                }
+                else
+                {
+                    y_ = 0;
+                }
             }
         }
         
         // Eventually: Writing the Character!
-        tmp_stat = GLCD_L3_WriteBitmap(pglcd_, tmp_pdata+1, x_, y_, *(tmp_pdata), font_->height, false);
+        HAL_StatusTypeDef tmp_stat = HAL_OK;
+        tmp_stat = GLCD_L3_WriteBitmap(pglcd_, tmp_pfdata+1, x_, y_, *(tmp_pfdata), font_->height, false);
         if(tmp_stat != HAL_OK)
         {
             // It means a more serious problem occured
             // (e.g. font is bigger than whole screen!)
             return tmp_stat;
         }
-
-        str_++;
-        x_ += *tmp_pdata;
+        
+        x_ += *tmp_pfdata;
     }
 
     return HAL_OK;
