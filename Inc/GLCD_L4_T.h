a2f7c5616e3b5147728a16ffd003df4f033ece7b
Parham <parhamk75@hotmail.com>
2020-11-28 13:30:42 +0330

    DEV: GLCD_L4_T: GLCD_L4_T_TrnsprntWriteChar/StringXY() dev and tested > GLCD_L4_G: Define some graphical shapes

--- Inc/GLCD_L4_T.h
+++ Inc/GLCD_L4_T.h
@@ -34,8 +34,8 @@ HAL_StatusTypeDef   GLCD_L4_T_WriteCharLine     (GLCD_L1_TypeDef* pglcd_, uint16
 HAL_StatusTypeDef   GLCD_L4_T_WriteStringLine   (GLCD_L1_TypeDef* pglcd_, uint16_t* str_, GLCD_L4_T_Font_TypeDef* font_, uint8_t line_, uint8_t y_, uint8_t len_, bool one_line_, bool cut_off_);
 
 // Transparent Write Funcs
-HAL_StatusTypeDef   GLCD_L4_T_TrnsprntWriteCharXY   (GLCD_L1_TypeDef* pglcd_, uint16_t char_, GLCD_L4_T_Font_TypeDef* font_, uint8_t x_, uint8_t y_, GLCD_L2_DispColor_TypeDef clr_);
-HAL_StatusTypeDef   GLCD_L4_T_TrnsprntWriteStringXY (GLCD_L1_TypeDef* pglcd_, uint16_t* str_, GLCD_L4_T_Font_TypeDef* font_, uint8_t x_, uint8_t y_, GLCD_L2_DispColor_TypeDef clr_, uint8_t len_, bool one_line_, bool cut_off_);
+HAL_StatusTypeDef   GLCD_L4_T_TrnsprntWriteCharXY   (GLCD_L1_TypeDef* pglcd_, uint32_t char_, GLCD_L4_T_Font_TypeDef* font_, uint8_t x_, uint8_t y_, GLCD_L2_DispColor_TypeDef clr_);
+HAL_StatusTypeDef   GLCD_L4_T_TrnsprntWriteStringXY (GLCD_L1_TypeDef* pglcd_, void* str_, GLCD_L4_Data_TypeDef dt_, GLCD_L4_T_Font_TypeDef* font_, uint8_t x_, uint8_t y_, GLCD_L2_DispColor_TypeDef clr_, uint8_t len_, bool one_line_, bool cut_off_);
 
 HAL_StatusTypeDef   GLCD_L4_T_TrnsprntWriteCharLine     (GLCD_L1_TypeDef* pglcd_, uint16_t char_, GLCD_L4_T_Font_TypeDef* font_, uint8_t line_, uint8_t y_, GLCD_L2_DispColor_TypeDef clr_);
 HAL_StatusTypeDef   GLCD_L4_T_TrnsprntWriteStringLine   (GLCD_L1_TypeDef* pglcd_, uint16_t* str_, GLCD_L4_T_Font_TypeDef* font_, uint8_t line_, uint8_t y_, GLCD_L2_DispColor_TypeDef clr_, uint8_t len_, bool one_line_, bool cut_off_);
