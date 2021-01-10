07d592926c92820542ad4ee9782e46780c990c9a
Parham <parhamk75@hotmail.com>
2021-01-10 08:53:51 +0330

    UPDATE: Organizing modules: GLCD_L4 -> GLCD_Module, GLCD_Modules.h added > modify contents to match the names and file structures

---
+++ Inc/GLCD_Modules.h
@@ -0,0 +1,12 @@
+#ifndef __GLCD_MODULES_H
+#define __GLCD_MODULES_H
+
+
+#ifndef __GLCD_MODULE_TEXT_H
+#define __GLCD_MODULE_TEXT_H
+#include "GLCD_Module_Text.h"
+#endif  //__GLCD_MODULE_TEXT_H
+
+
+
+#endif  //__GLCD_MODULES_H
